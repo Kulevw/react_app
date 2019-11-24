dad6ddce22de2d8d8d5b7b0b83fcc0d8b88b7f22
ishaburov <nkfrez@gmail.com>
2019-11-24 14:37:38 +0500

    fix date

--- src/components/Messanger/Messege.jsx
+++ src/components/Messanger/Messege.jsx
@@ -8,7 +8,7 @@ export class Messege extends Component {
             <Box mb={1} boxShadow={1}> <Card> <Box p={2}>
                 <Box borderBottom={1} mb={1} display="flex" justifyContent="space-between">
                     <div>{this.props.message.author}</div>
-                    <div>{moment(this.props.message.created_at).format('D.M.Y H:M:s')}</div>
+                    <div>{moment(this.props.message.created_at).format('D.M.Y H:M:ss')}</div>
                 </Box>
                 <div>
                     {this.props.message.text}
